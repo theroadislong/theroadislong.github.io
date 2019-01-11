415d2dd7356933ee314023833df295069054c5d8
Artem <dunaev.aa@yandex.ru>
2019-01-11 14:20:15 +0500

    fixes

--- csssr/js/script.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  const textAreaElement = document.querySelector(".essay__text");
-  textAreaElement.addEventListener('input', () => {
-    textAreaElement.style.height = 'auto';
-    textAreaElement.style.height = textAreaElement.scrollHeight + 'px';
-  });
-}())
