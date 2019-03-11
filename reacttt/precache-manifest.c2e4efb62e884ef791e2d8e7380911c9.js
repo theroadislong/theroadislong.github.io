ee5d4fbbd096f0f7b1ee552b28d0955751b24313
Artem Dunaev <dua@crtweb.ru>
2019-03-11 14:04:05 +0500

    add smth

---
+++ reacttt/precache-manifest.c2e4efb62e884ef791e2d8e7380911c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "b2ceef7fcbd35532b1b5",
+    "url": "/react/static/js/runtime~main.b2ceef7f.js"
+  },
+  {
+    "revision": "d78efc0f95e31474b63b",
+    "url": "/react/static/js/main.d78efc0f.chunk.js"
+  },
+  {
+    "revision": "092883a6cf5116e19494",
+    "url": "/react/static/js/2.092883a6.chunk.js"
+  },
+  {
+    "revision": "d78efc0f95e31474b63b",
+    "url": "/react/static/css/main.47bdd60a.chunk.css"
+  },
+  {
+    "revision": "092883a6cf5116e19494",
+    "url": "/react/static/css/2.690a8613.chunk.css"
+  },
+  {
+    "revision": "8191ded5a6ab602b3d971e47626ec9c6",
+    "url": "/react/index.html"
+  }
+];
